8fa613a0aaf6dbe73fe137871231b7230bd857e7
CaioCarvalho <135462331+CaiioCarvalho@users.noreply.github.com>
2025-01-29 19:33:11 -0400

    First thymeleaf contact

---
+++ src/main/java/br/com/caiocarvalho/front_gestao_vagas/PrimeiraPaginaController.java
@@ -0,0 +1,15 @@
+package br.com.caiocarvalho.front_gestao_vagas;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class PrimeiraPaginaController {
+
+    @GetMapping("/home")
+    public String primeiraPaginaHtml() {
+        return "primeiraPagina";
+    }
+}
