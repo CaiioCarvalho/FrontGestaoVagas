74c37ab7e28c76503b383d13c10da2683aefc3ef
CaioCarvalho <135462331+CaiioCarvalho@users.noreply.github.com>
2025-01-29 20:56:49 -0400

    passing info through thymeleaf

--- src/main/java/br/com/caiocarvalho/front_gestao_vagas/PrimeiraPaginaController.java
+++ src/main/java/br/com/caiocarvalho/front_gestao_vagas/PrimeiraPaginaController.java
@@ -3,6 +3,7 @@ package br.com.caiocarvalho.front_gestao_vagas;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,5 +15,16 @@ public class PrimeiraPaginaController {
 
         model.addAttribute("msgController", "Eis me aqui Senhor");
         return "primeiraPagina";
+    }
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "candidate/login";
+    }
+
+    @PostMapping("/create")
+    public String cadastroCandidate(String nome_do_candidato) {
+        System.out.println("Nome do candidato é:" + nome_do_candidato);
+        return "/candidate/login";
     }
 }
